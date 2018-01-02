6c672561b08032e812bf13d2ab4bfa2de5a18cd8
parsehd <parseh.research@gmail.com>
2018-01-02 10:26:30 +0330

    Still Initial

--- app/src/main/java/com/dparseh/samiee/shaderecognition/MainActivity.java
+++ app/src/main/java/com/dparseh/samiee/shaderecognition/MainActivity.java
@@ -99,6 +99,7 @@ public class MainActivity extends Activity implements BottomNavigation.OnMenuIte
                 Log.e("markerPosx: ", markerPosXY[0]+"");
                 Log.e("markerPosy: ", markerPosXY[1]+"");
 
+
                 printLab(xPos, yPos);
 
 //                float[] color = new float[3];
